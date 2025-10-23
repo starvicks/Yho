88dbae1845b60333dc0cd6521719f494d80370ce
starvicks <benjaminchukwunonso31@gmail.com>
2025-10-23 16:27:46 +0100

    Update settings.py

--- tenfortowbe-main/tenfortow/tenfortow/settings.py
+++ tenfortowbe-main/tenfortow/tenfortow/settings.py
@@ -142,7 +142,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'chimasid7@gmail.com'
 EMAIL_HOST_PASSWORD = 'kszihldzhtfeccyi'
